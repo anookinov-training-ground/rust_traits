65bfcfd22f85d02b2dd0a3338d5f6417c2d84c11
Alexander Nookinov <anookinov@gmail.com>
2021-08-21 23:11:45 +0700

    add trait bounds

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use std::fmt::Display;
+use std::fmt::Debug;
+
 pub trait Summary {
     fn summarize_author(&self) -> String;
 
@@ -32,4 +35,47 @@ impl Summary for Tweet {
         // format!("{}: {}", self.username, self.content)
         format!("@{}", self.username)
     }
+}
+
+// traits as parameters - impl Trait syntax
+pub fn notify(item: &impl Summary) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+// trait bound
+pub fn notify_generic<T: Summary>(item: &T) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+pub fn notify_two(item1: &impl Summary, item2: &impl Summary) {
+    println!("Breaking news! {}", item1.summarize());
+    println!("Breaking news! {}", item2.summarize());
+}
+
+pub fn notify_two_generic<T: Summary>(item1: &T, item2: &T) {
+    println!("Breaking news! {}", item1.summarize());
+    println!("Breaking news! {}", item2.summarize());
+}
+
+// multiple trait bounds with the + syntax
+pub fn notify_multiple(item: &(impl Summary + Display)) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+pub fn notify_multiple_generic<T: Summary + Display>(item: &T) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+pub fn some_function<T: Display + Clone, U: Clone + Debug>(t: &T, u: &U) -> i32 {
+    println!("{}, {:#?}", t, u);
+    10
+}
+
+// clearer trait bounds with where clauses
+pub fn some_function_where<T, U>(t: &T, u: &U) -> i32
+    where   T: Display + Clone,
+            U: Clone + Debug
+{
+    println!("{}, {:#?}", t, u);
+    10
 }
