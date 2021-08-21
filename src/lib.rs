dfcf865128331afd119973915b25fa8c1417f572
Alexander Nookinov <anookinov@gmail.com>
2021-08-21 23:51:20 +0700

    complete traits

--- src/lib.rs
+++ src/lib.rs
@@ -79,3 +79,67 @@ pub fn some_function_where<T, U>(t: &T, u: &U) -> i32
     println!("{}, {:#?}", t, u);
     10
 }
+
+// returning types that implement traits
+pub fn returns_summarizable() -> impl Summary {
+    Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    }
+}
+
+// not allowed to return multiple types with impl Trait
+// pub fn returns_summarizable_multiple(switch: bool) -> impl Summary {
+//     if switch {
+//         NewsArticle {
+//             headline: String::from(
+//                 "Penguins win the Stanley Cup Championship!",
+//             ),
+//             location: String::from("Pittsburgh, PA, USA"),
+//             author: String::from("Iceburgh"),
+//             content: String::from(
+//                 "The Pittsburgh Penguins once again are the best \
+//                  hockey team in the NHL.",
+//             ),
+//         }
+//     } else {
+//         Tweet {
+//             username: String::from("horse_ebooks"),
+//             content: String::from(
+//                 "of course, as you probably already know, people",
+//             ),
+//             reply: false,
+//             retweet: false,
+//         }
+//     }
+// }
+
+pub struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest number is y = {}", self.y);
+        }
+    }
+}
+
+// blanket implementations - implementations of a trait on any type that satisfies the trait bounds
+// impl<T: Display> ToString for T {
+//     // --snip--
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,31 @@
 use traits::{self, Summary, Tweet, NewsArticle};
 
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+    
+    largest
+}
+
 fn main() {
+    let s = 3.to_string();
+    println!("{}", s);
+
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
+
     let tweet = Tweet {
         username: String::from("horse_ebooks"),
         content: String::from(
